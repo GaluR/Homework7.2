4a5a7d3445a65bb962038fcfafedd803fab6f58c
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-25 13:49:08 +0200

    Change loop while adn do/while

--- src/Counter.java
+++ src/Counter.java
@@ -1,11 +1,11 @@
 public class Counter {
     public static void main(String[] args) {
         double number=0;
-
-        while(number<=3.1){
+        while(number<=3.0){
             System.out.printf("%.1f, ", number);
             number=number+0.1;
         }
+        System.out.println("3.0");
     }
 }
 
